6b6ff4dc55869f5d2220633f775362b3da798bfb
Melker Ulander <melker.ulander@pm.me>
2026-06-09 20:20:45 +0200

    Move some functions to Application

---
+++ src/application.rs
@@ -0,0 +1,33 @@
+use std::marker::PhantomData;
+
+use anyhow::Context;
+use clap::Parser;
+
+use crate::{cli, compositor::Compositor};
+
+pub(crate) struct Application<C: Compositor>(PhantomData<C>);
+
+impl<C: Compositor> Application<C> {
+    pub(crate) fn run() -> anyhow::Result<()> {
+        let args = cli::Args::parse();
+
+        let search_string = args.app_id.as_deref().unwrap_or(&args.app).to_lowercase();
+        let sibling_windows =
+            C::get_window_group(search_string).context("failed to get window group")?;
+
+        if sibling_windows.is_empty() {
+            C::launch_application(&args.app)?;
+            return Ok(());
+        }
+
+        let target_window = Self::get_cycle_window_target(&sibling_windows);
+        C::focus_window(target_window).context("failed to focus window")?;
+
+        Ok(())
+    }
+
+    fn get_cycle_window_target(sibling_windows: &[C::Win]) -> &C::Win {
+        let idx = if sibling_windows.len() >= 2 { 1 } else { 0 };
+        &sibling_windows[idx]
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,18 @@
-use anyhow::Context;
-use clap::Parser;
-use compositor::Compositor;
+use crate::application::Application;
 
 #[cfg(feature = "compositor-hyprland")]
 use crate::compositor::integrations::hyprland::HyprlandCompositor;
 #[cfg(feature = "compositor-niri")]
 use crate::compositor::integrations::niri::NiriCompositor;
 
+mod application;
 mod cli;
 mod compositor;
 
-fn run<C: Compositor>() -> anyhow::Result<()> {
-    let args = cli::Args::parse();
-
-    let search_string = args.app_id.as_deref().unwrap_or(&args.app).to_lowercase();
-    let sibling_windows =
-        C::get_window_group(search_string).context("failed to get window group")?;
-
-    if sibling_windows.is_empty() {
-        C::launch_application(&args.app)?;
-        return Ok(());
-    }
-
-    // `get_windows` returns windows sorted most-recently-focused first.
-    // Pick the second-most-recent window, or the only one if alone.
-    let target_window = if sibling_windows.len() >= 2 {
-        &sibling_windows[1]
-    } else {
-        &sibling_windows[0]
-    };
-
-    C::focus_window(target_window).context("failed to focus window")?;
-
-    Ok(())
-}
-
 fn main() -> anyhow::Result<()> {
     #[cfg(feature = "compositor-hyprland")]
-    return run::<HyprlandCompositor>();
+    return Application::<HyprlandCompositor>::run();
 
     #[cfg(feature = "compositor-niri")]
-    return run::<NiriCompositor>();
+    return Application::<NiriCompositor>::run();
 }
